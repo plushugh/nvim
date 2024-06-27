a7e23db5d568852cb7d8f4a63c9d1aee10341553
Hugh He <plushugh@gmail.com>
2024-06-27 15:04:57 -0400

    feat: add files via upload

---
+++ lua/config/options.lua
@@ -0,0 +1,21 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+if vim.g.neovide then
+  vim.g.neovide_text_gamma = 0.8
+  vim.g.neovide_text_contrast = 0.1
+
+  vim.g.neovide_transparency = 1.0
+
+  vim.g.neovide_window_blurred = true
+  vim.g.neovide_floating_blur_amount_x = 2.0
+  vim.g.neovide_floating_blur_amount_y = 2.0
+  vim.g.neovide_floating_shadow = true
+  vim.g.neovide_floating_z_height = 10
+  vim.g.neovide_light_angle_degrees = 45
+  vim.g.neovide_light_radius = 5
+
+  vim.g.neovide_show_border = true
+
+  vim.g.neovide_input_macos_option_key_is_meta = "only_left"
+end
